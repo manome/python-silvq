b8bc19a4c2c5a5e39e3fbec5dd92209bd2a5f654
manome <manome@users.noreply.github.com>
2021-04-08 17:23:46 +0900

    Update plot2d_artificial_dataset1_silvq

--- plot2d_artificial_dataset1_silvq.py
+++ plot2d_artificial_dataset1_silvq.py
@@ -14,7 +14,7 @@ def main():
     x = dataset[:, :-1].astype('float64')
     y = dataset[:, -1].astype('int64')
     # Split dataset into training set and test set
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=8, shuffle=True, stratify=y)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=3, shuffle=True, stratify=y)
 
     # Generating model
     model = SilvqModel(x.shape[1], theta=0.8, bias_type='ls')
